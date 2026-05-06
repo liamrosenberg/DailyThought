baea47e7ad8094c4c9d5ad0e2fe49b4e9e07fdcc
liamrosenberg <liam.rosenberg@gmail.com>
2026-05-06 13:39:37 -0400

    Connected to database

---
+++ daily-thoughts-app/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// You will get these from your Supabase dashboard
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
